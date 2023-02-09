337ad4fd53e633005e7dc00d929599468ce3193e
2realzoo <lunalee00@gmail.com>
2023-02-09 15:11:28 +0900

    create, update 구현 중

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { dateFilter } from "./filters";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos }) => {
+    const { dateReducer } = useSelector(state => state);
+    todos = dateFilter(todos , dateReducer)
     return (
         <div className="todolist-wrapper">
             {todos.map(todo => 
